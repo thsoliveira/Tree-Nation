96c223dc1f227b8589bc17c200fe5c5556af47c1
Thais Oliveira <thaisoliveira@Thaiss-MacBook-Pro.local>
2026-06-05 21:39:46 +0700

    fix: 404 redirect and cors proxy

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,6 +1,6 @@
 export const API_BASE_URL = import.meta.env.DEV
 	? "/api"
-	: "https://youcannevertestenough.tree-nation.com/";
+	: "https://api.allorigins.win/raw?url=https://youcannevertestenough.tree-nation.com/";
 export const THUMBNAIL_BASE_URL =
 	"https://youcannevertestenough.tree-nation.com/thumbs";
 export const DEFAULT_THUMBNAIL_SIZE = "716x0";
